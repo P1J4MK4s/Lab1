bd0e378f765d238f88452a4f405035f8506e9d6a
Max Pijevskiy <52789371+P1J4MK4s@users.noreply.github.com>
2020-03-20 12:50:33 +0300

    Сделал первую лабораторную работу
    Кто-то вообще читает коментраии к комиту?

---
+++ Код.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        enum gradus : int
+        {
+            min = 0,
+            krit = 72,
+            max = 100,
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*const*/ int i = 10;
+            Console.WriteLine(i);
+            i = 100;
+            Console.WriteLine(i);
+            Console.WriteLine("минимальная температура=" + (int)gradus.min);
+            Console.WriteLine("критическая температура=" + (int)gradus.krit);
+            Console.WriteLine("максимальная температура=" + (int)gradus.max);
+
+            double x = Math.E;
+            Console.WriteLine("E={0:##.###}", x);
+            Console.WriteLine("E={0:.####}", x);
+            Console.WriteLine("E={0:.######}", x);
+
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine("c={0:.###}",c);
+
+
+
+        }
+    }
+}
